d03f91ca0fcb1a1ceb7c56616a1741aa86a5f724
James Quick <james.q.quick@outlook.com>
2019-08-13 20:57:36 -0500

    Updated site title

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,9 +4,9 @@ module.exports = {
     url: "http://localhost:8888",
   },
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `We Belong`,
+    description: `A curated list of stories to help encourage others fighting Imposter Syndrome`,
+    author: `@jamesqquick`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
